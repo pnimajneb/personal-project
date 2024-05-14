ec976297b8ae32fbd2229acfd12612e771223318
benjaminp <bpeters@posteo.de>
2024-05-14 13:08:19 +0200

    implementing a working function for displaying certain images

--- src/app/(home)/messages/ImageList.tsx
+++ src/app/(home)/messages/ImageList.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { getImages } from "../../../../lib/imageService";
 
 export default async function ImageList() {
-  const images = await getImages();
+  const imagesGallery = await getImages();
   return (
     <>
-      {images.map((image, index) => (
-        <div key={index} className="my-5">
-          <img src={image.url} alt={image.name} />
-        </div>
-      ))}
-      {images.length === 0 && (
+      {imagesGallery.map(
+        (img, index) =>
+          img.name.includes("aboutme") && (
+            <div key={index} className="my-5">
+              <img src={img.url} alt={img.name} />
+            </div>
+          )
+      )}
+      {imagesGallery.length === 0 && (
         <p className="text-center">There are no images here!</p>
       )}
     </>
