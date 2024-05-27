2bbb4577813e3eed0aa34ebf61ef0a6d4b198e56
benjaminp <bpeters@posteo.de>
2024-05-27 16:06:19 +0200

    implementing tailwind grid on about page

--- src/app/(home)/about/content.tsx
+++ src/app/(home)/about/content.tsx
@@ -8,38 +8,37 @@ export async function Content() {
       {aboutImage.map(
         (img, index) =>
           img.name.includes("aboutme") && (
-            <div className="max-w-[200px] w-full">
-              <div className="float-right" key={index}>
-                <div className="flex">
-                  <img className="rounded-[4px]" src={img.url} alt={img.name} />
-                  <h1 className="pl-10">LEA DINGER</h1>
-                </div>
-                <p className="text-[#a0aec0] mt-6">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
-                  pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                  In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
-                  justo. Nullam dictum felis eu pede mollis pretium. Integer
-                  tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
-                  vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
-                  consequat vitae, eleifend ac, enim. Aliquam lorem ante,
-                  dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
-                  nulla ut metus varius laoreet. Quisque rutrum. Aenean
-                  imperdiet. Etiam ultricies nisi vel augue. Curabitur
-                  ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.
-                  Maecenas tempus, tellus eget condimentum rhoncus, sem quam
-                  semper libero, sit amet adipiscing sem neque sed ipsum. Nam
-                  quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-                  Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien
-                  ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet
-                  orci eget eros faucibus tincidunt. Duis leo. Sed fringilla
-                  mauris sit amet nibh. Donec sodales sagittis magna. Sed
-                  consequat, leo eget bibendum sodales, augue velit cursus nunc,
-                </p>
-              </div>
+            <div key={index} className="grid grid-cols-2 w-[400px]">
+              <img
+                className="rounded-[4px] max-w-[200px]"
+                src={img.url}
+                alt={img.name}
+              />
+              <h1 className="ml-4">LEA DINGER</h1>
+              <p className="text-[#a0aec0] col-span-2 mt-4">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                quis, sem. Nulla consequat massa quis enim. Donec pede justo,
+                fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
+                felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+                Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+                Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+                enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,
+                tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque
+                rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
+                Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam
+                rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem
+                quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam
+                quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
+                Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien
+                ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet
+                orci eget eros faucibus tincidunt. Duis leo. Sed fringilla
+                mauris sit amet nibh. Donec sodales sagittis magna. Sed
+                consequat, leo eget bibendum sodales, augue velit cursus nunc,
+              </p>
             </div>
           )
       )}
